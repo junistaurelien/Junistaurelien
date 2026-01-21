35737adb4e524ce43f07cf87610cc1d0cfe50eb1
Junist Aurelien <junistaurelien@gmail.com>
2026-01-21 15:17:11 -0500

    Create WN10-CC-000090.ps1

---
+++ STIGS/WN10-CC-000090.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+   
+
+.NOTES
+    Author          : Junist Aurelien
+    LinkedIn        : www.linkedin.com/in/junist-aurelien-cysa-0b96141ab
+    GitHub          : https://github.com/junistaurelien/
+    Date Created    : 2026-1-21
+    Last Modified   : 2026-1-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : (WN10-CC-000090)
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run Using PowerShell!
+   
+#>
+
+
+# YOUR CODE GOES HERE WN10-CC-000090
+#Requires #Requires -RunAsAdministrator
+$path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Group Policy\{35378EAC-683F-11D2-A89A-00C04FBBCFA2}'
+$name = 'NoGPOListChanges'
+$value = 0
+if (-not (Test-Path $path)) { New-Item -Path $path -Force | Out-Null }
+New-ItemProperty -Path $path -Name $name -PropertyType DWord -Value $value -Force | Out-Null
+
