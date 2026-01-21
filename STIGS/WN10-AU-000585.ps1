f8a17b3284f8fb9d55f8267add7880a9ea838ecd
Junist Aurelien <junistaurelien@gmail.com>
2026-01-21 15:21:57 -0500

    Create WN10-AU-000585.ps1

---
+++ STIGS/WN10-AU-000585.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+   
+
+.NOTES
+    Author          : Junist Aurelien
+    LinkedIn        : www.linkedin.com/in/junist-aurelien-cysa-0b96141ab
+    GitHub          : https://github.com/junistaurelien/
+    Date Created    : 2026-1-21
+    Last Modified   : 2026-1-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : (WN10-AU-000585)
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run Using PowerShell!
+   
+#>
+
+
+# YOUR CODE GOES HERE WN10-AU-000585 
+#Requires #Requires -RunAsAdministrator
+auditpol /set /subcategory:"Process Creation" /failure:enable | Out-Null
+
