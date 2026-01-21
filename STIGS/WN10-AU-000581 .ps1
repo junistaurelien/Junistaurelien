8ff0290e759da0316deede58f1d0415746ddae4c
Junist Aurelien <junistaurelien@gmail.com>
2026-01-21 15:30:05 -0500

    Create WN10-AU-000581 .ps1

---
+++ STIGS/WN10-AU-000581 .ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS (Audit File System (Failure)
+   
+
+.NOTES
+    Author          : Junist Aurelien
+    LinkedIn        : www.linkedin.com/in/junist-aurelien-cysa-0b96141ab
+    GitHub          : https://github.com/junistaurelien/
+    Date Created    : 2026-1-21
+    Last Modified   : 2026-1-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : (-AU-000581 )
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run Using PowerShell!
+   
+#>
+
+
+# WN10-AU-000581 
+
+#Requires -RunAsAdministrator
+auditpol /set /subcategory:"File System" /failure:enable | Out-Null
