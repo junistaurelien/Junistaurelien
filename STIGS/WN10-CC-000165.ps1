cbfacf75a15d065ffbab49f1436c8693de562d7d
Junist Aurelien <junistaurelien@gmail.com>
2026-01-21 15:26:48 -0500

    Create WN10-CC-000165.ps1

---
+++ STIGS/WN10-CC-000165.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS (Set legal banner title (Restrict unauthenticated RPC clients (RestrictRemoteClients=1))
+   
+
+.NOTES
+    Author          : Junist Aurelien
+    LinkedIn        : www.linkedin.com/in/junist-aurelien-cysa-0b96141ab
+    GitHub          : https://github.com/junistaurelien/
+    Date Created    : 2026-1-21
+    Last Modified   : 2026-1-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : (WN10-CC-000165  )
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Run Using PowerShell!
+   
+#>
+
+
+# YOUR CODE GOES HERE WN10-CC-000165 
+
+#Requires -RunAsAdministrator
+$path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Rpc'
+$name = 'RestrictRemoteClients'
+$value = 1
+if (-not (Test-Path $path)) { New-Item -Path $path -Force | Out-Null }
+New-ItemProperty -Path $path -Name $name -PropertyType DWord -Value $value -Force | Out-Null
